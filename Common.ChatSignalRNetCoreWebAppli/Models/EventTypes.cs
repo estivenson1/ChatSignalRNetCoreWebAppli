0afe791d5e016bf41adabcfbfb98439e09cf55dc
EsOrVi <csharp_eov@hotmail.com>
2019-12-11 05:57:34 -0500

    EO - Add EventTypes

---
+++ Common.ChatSignalRNetCoreWebAppli/Models/EventTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.ChatSignalRNetCoreWebAppli.Models
+{
+    public enum EventTypes
+    {
+        AddCounter=1,
+        EnableUser=2,
+    }
+
+    public class Menssage
+    {
+        EventTypes EventType;
+    }
+}
